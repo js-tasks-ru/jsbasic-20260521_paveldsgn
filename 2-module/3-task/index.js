3e1a12142e3986b759ad354a0a67cdb2a455c017
Pavel Makarchuk <paveldsgn@gmail.com>
2026-06-02 18:41:02 +0300

    решение задачи - калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read() {
+    this.a = +prompt('Введите первое число', '');
+    this.b = +prompt('Введите второе число', '');
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
