c8adf7a3fa43d9c8eae62e8f8fc4623c9323a31d
Pavel Makarchuk <paveldsgn@gmail.com>
2026-06-04 08:53:59 +0300

    перевод текста

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let splitedStr = str.split('-');
+  let result = splitedStr.map((firstStr, index) => {
+    if (index === 0) {
+      return firstStr;
+    }
+    return `${firstStr[0].toUpperCase()}${firstStr.slice(1)}`;
+  });
+  return result.join('');
 }
