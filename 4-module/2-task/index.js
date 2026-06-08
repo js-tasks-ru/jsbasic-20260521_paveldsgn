53af90ad808a0f4f5367361ccd86d189de6e101f
Pavel Makarchuk <paveldsgn@gmail.com>
2026-06-08 18:36:59 +0300

    Выделите ячейки по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,6 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for(let i = 0; i < table.rows.length; i++) {
+    // console.log(table.rows[i].cells[i]);
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
