ad5d01402954e1644954c55459ad36a40e14f05c
Pavel Makarchuk <paveldsgn@gmail.com>
2026-06-14 08:47:59 +0300

    Учебный проект (пролог): Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let slideContainer = document.querySelector('.carousel__inner');
+
+  arrowLeft.style.display = 'none';
+
+  let slideWidth = slideContainer.offsetWidth;
+  let currentSlide = 0;
+  let totalSlide = 4;
+
+
+    arrowRight.addEventListener('click', () => {
+      if (currentSlide < totalSlide - 1) {
+        arrowLeft.style.display = '';        
+        currentSlide++;
+        let shift = -currentSlide * slideWidth;   
+        slideContainer.style.transform = `translateX(${shift}px)`;
+      } 
+      if (currentSlide === totalSlide - 1) {
+        arrowRight.style.display = 'none';
+      }
+    });
+
+    arrowLeft.addEventListener('click', () => {
+      if (currentSlide > 0) {
+        arrowRight.style.display = '';
+        currentSlide--;
+        let shift = -currentSlide * slideWidth;
+        slideContainer.style.transform = `translateX(${shift}px)`;
+      }
+      if (currentSlide === 0) {
+        arrowLeft.style.display = 'none';
+      }
+    });
 }
